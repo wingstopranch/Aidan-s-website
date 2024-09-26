ea3dd0f3d0a681dd33a009253b4ae7cc4cd0c1ba
Aidan Ho Hayman <aidanho777@gmail.com>
2024-09-26 11:39:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Thanks, ${name}! Your message has been sent.`);
+});
